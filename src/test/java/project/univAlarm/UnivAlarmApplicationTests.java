429568d827cc16a4fe4564f8b2fae3ed636051fe
5ms <oms010414@gmail.com>
2025-07-24 18:49:36 +0900

    first commit

---
+++ src/test/java/project/univAlarm/UnivAlarmApplicationTests.java
@@ -0,0 +1,13 @@
+package project.univAlarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnivAlarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

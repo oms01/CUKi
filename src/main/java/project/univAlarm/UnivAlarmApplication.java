0ea4ccf0cf49fa6c9e909e0e45aba14330ac68fd
5ms <86279729+oms01@users.noreply.github.com>
2025-07-26 21:56:51 +0900

    Implement CatholicUniv All Crawling Logic
    * Implement CatholicUniv Default/Medicine/Nursing Crawler
    
    * Implement Notification Detector / DetectorManager

--- src/main/java/project/univAlarm/UnivAlarmApplication.java
+++ src/main/java/project/univAlarm/UnivAlarmApplication.java
@@ -2,8 +2,11 @@ package project.univAlarm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import project.univAlarm.config.UnivConfigProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(UnivConfigProperties.class)
 public class UnivAlarmApplication {
 
 	public static void main(String[] args) {
--- src/main/java/project/univAlarm/domain/Notification.java
+++ src/main/java/project/univAlarm/domain/Notification.java
@@ -1,12 +1,20 @@
 package project.univAlarm.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
-
-import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "notifications",

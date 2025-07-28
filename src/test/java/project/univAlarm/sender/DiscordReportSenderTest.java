309ab1479493a54173c181de64201ca12c685191
5ms <86279729+oms01@users.noreply.github.com>
2025-07-28 19:12:45 +0900

    Implement FCM, Discrod Notification Sender
    * Implement FCM, Discord Sender
    
    * Implement testcode for FcmNotificationSender
    
    * Implement testcode for DiscordReportSender

---
+++ src/test/java/project/univAlarm/sender/DiscordReportSenderTest.java
@@ -0,0 +1,42 @@
+package project.univAlarm.sender;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Mono;
+
+@SpringBootTest
+class DiscordReportSenderTest {
+
+    @Autowired
+    private DiscordReportSender discordReportSender;
+
+    @Test
+    public void discordReportSenderTest() {
+        PushNotificationReport dummyPushNotificationReport = createDummyPushNotificationReport();
+        Mono<Boolean> result = discordReportSender.send(dummyPushNotificationReport);
+        System.out.println(result.block());
+    }
+
+    private PushNotificationReport createDummyPushNotificationReport() {
+        PushNotificationReport dummyPushNotificationReport = new PushNotificationReport();
+        dummyPushNotificationReport.setNotification(createDummyPushNotificationDto());
+        dummyPushNotificationReport.setSuccessCount(8);
+        dummyPushNotificationReport.setFailureCount(2);
+        dummyPushNotificationReport.setTotalCount(10);
+        return dummyPushNotificationReport;
+    }
+
+    private PushNotificationDto createDummyPushNotificationDto() {
+        PushNotificationDto pushNotificationDto = new PushNotificationDto();
+        pushNotificationDto.setSchoolName("가톨릭대학교/성심교정");
+        pushNotificationDto.setKind("장학");
+        pushNotificationDto.setTitle("[학생지원팀](한국장학재단)2025-2 국가근로장학생 선발 관련 재단/트리니티 희망근로지 신청 안내(8/1~8/11)\n");
+        pushNotificationDto.setDate("2025.07.28");
+        pushNotificationDto.setWriter("학생지원팀");
+        pushNotificationDto.setLink("https://www.catholic.ac.kr/ko/campuslife/notice.do?mode=view&articleNo=262710&article.offset=0&articleLimit=10");
+        return pushNotificationDto;
+    }
+}

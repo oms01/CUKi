a781710d241838cbb87f0cac6faebeae51be957c
5ms <oms010414@gmail.com>
2025-08-03 19:59:17 +0900

    Implement SubscriptionController

---
+++ src/main/java/project/univAlarm/controller/dto/CreateSubscriptionRequestDto.java
@@ -0,0 +1,10 @@
+package project.univAlarm.controller.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateSubscriptionRequestDto {
+    private Long notificationTypeId;
+}

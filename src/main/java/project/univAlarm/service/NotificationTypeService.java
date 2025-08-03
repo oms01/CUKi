6fa7a668afd30c06e57772df822ed926fe35d804
5ms <oms010414@gmail.com>
2025-08-03 20:04:17 +0900

    Implement SchoolController

--- src/main/java/project/univAlarm/service/NotificationTypeService.java
+++ src/main/java/project/univAlarm/service/NotificationTypeService.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import project.univAlarm.domain.NotificationType;
-import project.univAlarm.domain.School;
 import project.univAlarm.repository.NotificationTypeRepository;
 import project.univAlarm.service.dto.NotificationTypeResponseDto;
 
@@ -15,7 +14,7 @@ public class NotificationTypeService {
     private final NotificationTypeRepository notificationTypeRepository;
 
     @Transactional(readOnly = true)
-    public List<NotificationTypeResponseDto> findBySchool(School school) {
+    public List<NotificationTypeResponseDto> findBySchool(Long school) {
         List<NotificationType> notificationTypes = notificationTypeRepository.findBySchool(school);
         return notificationTypes.stream()
                 .map(NotificationTypeResponseDto::new)

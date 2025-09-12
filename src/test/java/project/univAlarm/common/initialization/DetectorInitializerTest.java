d866ee81ff31d33a352530d7f580e4a7d52c00b8
5ms <oms010414@gmail.com>
2025-09-13 01:19:14 +0900

    Implement test

---
+++ src/test/java/project/univAlarm/common/initialization/DetectorInitializerTest.java
@@ -0,0 +1,63 @@
+package project.univAlarm.common.initialization;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import project.univAlarm.common.detector.NotificationDetector;
+import project.univAlarm.common.initialization.dto.SimpleNotificationDto;
+import project.univAlarm.common.initialization.dto.SimpleNotificationTypeDto;
+import project.univAlarm.common.initialization.dto.SimpleSchoolDto;
+
+@SpringBootTest
+class DetectorInitializerTest {
+
+    @Autowired
+    private DetectorInitializer detectorInitializer;
+
+    @Test
+    void 기존_순차_크롤링() throws IOException {
+        int iterations = 100;
+        long totalTime = 0;
+
+        for (int i = 0; i < iterations; i++) {
+
+            long start = System.currentTimeMillis();
+            List<SimpleNotificationDto> c = detectorInitializer.init();
+            long end = System.currentTimeMillis();
+
+            long elapsed = end - start;
+            totalTime += elapsed;
+
+            System.out.println("Iteration " + (i + 1) + " 실행 시간: " + elapsed + "ms");
+        }
+
+        double average = totalTime / (double) iterations;
+        System.out.println("평균 실행 시간: " + average + "ms");
+    }
+
+    @Test
+    void 병렬_크롤링() throws IOException {
+        int iterations = 100;
+        long totalTime = 0;
+
+        for (int i = 0; i < iterations; i++) {
+
+            long start = System.currentTimeMillis();
+            List<SimpleNotificationDto> c = detectorInitializer.initV2();
+            long end = System.currentTimeMillis();
+
+            long elapsed = end - start;
+            totalTime += elapsed;
+
+            System.out.println("Iteration " + (i + 1) + " 실행 시간: " + elapsed + "ms");
+        }
+
+        double average = totalTime / (double) iterations;
+        System.out.println("평균 실행 시간: " + average + "ms");
+    }
+}

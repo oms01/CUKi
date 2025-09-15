1e261630ccf01c1b3c84bc7b2ac3fa1a5ebc4255
5ms <oms010414@gmail.com>
2025-09-15 10:33:19 +0900

    fix sql error in NotificationService

--- src/main/java/project/univAlarm/notification/service/NotificationService.java
+++ src/main/java/project/univAlarm/notification/service/NotificationService.java
@@ -45,7 +45,7 @@ public class NotificationService {
     }
 
     public boolean isExist(Long notificationTypeId, Long notificationOriginId) {
-        String sql = "SELECT notification_type_id FROM notification WHERE notification_type_id = ? AND origin_id = ?";
+        String sql = "SELECT notification_type_id FROM notifications WHERE notification_type_id = ? AND origin_id = ?";
         Long id = jdbcTemplate.queryForObject(sql, Long.class, notificationOriginId, notificationTypeId);
         return id != null;
     }
